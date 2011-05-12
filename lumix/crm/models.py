e842924e00fc167a787684114b5993f02fd2388e
Carlos Ribeiro <carribeiro@gmail.com>
2011-05-12 04:55:08 -0700

    Modelagem do controle de clientes/endereços/contratos, de acordo com os dados existentes, para início do processo de faturamento via sistema

--- lumix/crm/models.py
+++ lumix/crm/models.py
@@ -15,11 +15,11 @@ TIPO_RETENCAO_IMPOSTOS = (
 
 class Empresa(models.Model):
     empresa = models.CharField(verbose_name='nome da empresa', max_length=30, blank=False, unique=True, db_index=True)
-    porte = models.CharField(verbose_name='porte', max_length=15, choices=(('P', 'Pequeno'), ('M', 'Médio'), ('G', 'Grande')), blank=True, default='')
-    razao_social = models.CharField(verbose_name='razão social', max_length=60, blank=False, unique=True, db_index=True)
+    seq_designacao = models.IntegerField(verbose_name='sequência de designação', unique=True, null=True, db_index=True)
+    porte = models.CharField(verbose_name='porte', max_length=15,
+                choices=(('-', 'Desconhecido'), ('P', 'Pequeno'), ('M', 'Médio'), ('G', 'Grande')), 
+                blank=False, default='-')
     segmento = models.ForeignKey('Segmento', null=True) 
-    atividade = models.CharField(verbose_name='atividade', max_length=60, blank=True, default='')
-    data_captacao = models.DateField(verbose_name='data de captação')
     # os campos a seguir pode ter os valores: N (não), S (sim), M (somente NF municipal), E (somente NF estadual)
     retem_iss = models.CharField(verbose_name='retém ISS', max_length=1, choices=TIPO_RETENCAO_IMPOSTOS)
     retem_pis = models.CharField(verbose_name='retém PIS', max_length=1, choices=TIPO_RETENCAO_IMPOSTOS)
@@ -28,10 +28,12 @@ class Empresa(models.Model):
     retem_csll = models.CharField(verbose_name='retém CSLL', max_length=1, choices=TIPO_RETENCAO_IMPOSTOS)
     dispensa_icms = models.BooleanField(verbose_name='dispensa de ICMS')
     destaque_icms = models.BooleanField(verbose_name='destacar ICMS')
+    nf_separada = models.BooleanField(verbose_name='emitir NF separada')
 
 class Endereco(models.Model):
     cnpj = models.CharField(verbose_name='CNPJ do endereço', max_length=20, blank=False, unique=True, db_index=True)
     empresa = models.ForeignKey('Empresa')
+    razao_social = models.CharField(verbose_name='razão social', max_length=60, blank=False, unique=True, db_index=True)
     endereco = models.CharField(verbose_name='endereço', max_length=80, default="")
     bairro = models.CharField(verbose_name='bairro', max_length=40, default="")
     cidade = models.CharField(verbose_name='cidade', max_length=40, default="")
@@ -44,18 +46,24 @@ class Endereco(models.Model):
     # TODO: verificar se a inscrição estadual é única para a empresa ou é por 
     # endereço. a inscrição municipal eu já sei que é por endereço.
 
-class Contrato(models.Model):
-    cliente = models.ForeignKey('Endereco')
-
-class Aditivo(models.Model):
-    empresa = models.ForeignKey('Empresa')
-
 class Circuito(models.Model):
     designacao = models.CharField(verbose_name='designação do circuito', max_length=18, blank=False, unique=True, db_index=True)
     produto = models.ForeignKey('catalogo.Produto')
+    vlan = models.IntegerField(verbose_name='VLAN', null=True)
+    empresa = models.ForeignKey('Empresa')
 
-class Item(models.Model):
-    designacao = models.CharField(verbose_name='designação do circuito', max_length=18, blank=False, unique=True, db_index=True)
+class Contrato(models.Model):
+    empresa = models.ForeignKey('Empresa')
 
+class ItemContrato(models.Model):
+    contrato = models.ForeignKey('Contrato')
+    endereco_faturamento = models.ForeignKey('Endereco')
+    circuito = models.ForeignKey('Circuito')
+    info_adicional = models.CharField(verbose_name='informações adicionais', max_length=60, default="", blank=True)
+    valor = models.DecimalField(verbose_name='valor', max_digits=9, decimal_places=2)
+    prazo = models.IntegerField(verbose_name='prazo', default=36)
+    data_inicial = models.DateField(verbose_name='data inicial', null=True)
+    data_final = models.DateField(verbose_name='data final', null=True)
+    modalidade = models.CharField(verbose_name='modalidade', max_length=20, default="", blank=True)
+    ciclo = models.IntegerField(verbose_name='ciclo', default=15)
 
-    # continua amanhã
