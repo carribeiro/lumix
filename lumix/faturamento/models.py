380e43b1d11e07fcdbab0a6180b1bd4f9114a83a
Carlos Ribeiro <carribeiro@gmail.com>
2011-05-13 00:38:30 -0700

    Cálculo do faturamento (versao inicial)

--- lumix/faturamento/models.py
+++ lumix/faturamento/models.py
@@ -45,5 +45,111 @@ class ItemFaturado(models.Model):
     val_ret_iss = models.DecimalField(verbose_name='%ISS', max_digits=9, decimal_places=2)
     val_ret_ir = models.DecimalField(verbose_name='%IR', max_digits=9, decimal_places=2)
     val_ret_csll = models.DecimalField(verbose_name='%CSLL', max_digits=9, decimal_places=2)
-    valor_bruto = models.DecimalField(verbose_name='valor', max_digits=9, decimal_places=2)
+    valor_bruto = models.DecimalField(verbose_name='valor bruto', max_digits=9, decimal_places=2)
 
+#========================
+# http://code.activestate.com/recipes/577274-subtract-or-add-a-month-to-a-datetimedate-or-datet/
+
+"""
+def subtract_one_month(dt0):
+    dt1 = dt0.replace(days=1)
+    dt2 = dt1 - timedelta(days=1)
+    dt3 = dt2.replace(days=1)
+    return dt3
+
+def add_one_month(dt0):
+    dt1 = dt0.replace(days=1)
+    dt2 = dt1 + timedelta(days=32)
+    dt3 = dt2.replace(days=1)
+    return dt3
+
+def calcula_limite_ciclo(ciclo, mes, ano):
+    import datetime
+    result = {}
+    result['Mẽs Corrido'] = ()
+"""
+
+ciclos_maio = {
+    1: {
+        'Mês Corrido': ('01/04/2011', '30/04/2011'),
+        'Mês Fechado Pós': ('01/04/2011', '30/04/2011' ),
+        'Mês Fechado Pré': ('01/05/2011', '31/05/2011'),
+    },
+    9: {
+        'Mês Corrido': ('09/04/2011', '08/05/2011'),
+        'Mês Fechado Pós': ('01/04/2011', '30/04/2011' ),
+        'Mês Fechado Pré': ('01/05/2011', '31/05/2011'),
+    },
+    10: {
+        'Mês Corrido': ('10/04/2011', '09/05/2011'),
+        'Mês Fechado Pós': ('01/04/2011', '30/04/2011' ),
+        'Mês Fechado Pré': ('01/05/2011', '31/05/2011'),
+    },
+    15: {
+        'Mês Corrido': ('15/04/2011', '14/05/2011'),
+        'Mês Fechado Pós': ('01/04/2011', '30/04/2011' ),
+        'Mês Fechado Pré': ('01/05/2011', '31/05/2011'),
+    },
+    17: {
+        'Mês Corrido': ('17/04/2011', '16/05/2011'),
+        'Mês Fechado Pós': ('01/04/2011', '30/04/2011' ),
+        'Mês Fechado Pré': ('01/05/2011', '31/05/2011'),
+    },
+}   
+
+def calcula_faturamento():
+    ItemFaturado.objects.all().delete()
+    for pf in ProvisaoFatura.objects.all():
+        ifat = ItemFaturado(provisao_fatura=pf)
+        ifat.save()
+        ifat.data_inicial = pf.data_inicial
+        ifat.data_final = pf.data_final
+        ifat.inicio_ciclo, ifat.fim_ciclo= ciclos_maio[pf.ciclo][pf.modalidade]
+        #ifat.inicio_fatura
+        #ifat.fim_fatura
+
+        produto = pf.produto
+        empresa = pf.endereco_faturamento.empresa
+
+        # impostos diretos
+        ifat.valor_bruto = pf.valor
+        ifat.perc_pis = produto.pis
+        ifat.perc_cofins = produto.cofins
+        ifat.perc_iss = produto.iss
+        ifat.perc_icms = produto.icms
+        ifat.val_pis = pf.valor * produto.pis
+        ifat.val_cofins = pf.valor * produto.cofins
+        ifat.val_iss = pf.valor * produto.iss
+        ifat.val_icms = pf.valor * produto.icms
+
+        # retencoes
+        if (empresa.retem_pis == 'S') or (empresa.retem_pis == produto.esfera):
+            ifat.perc_ret_pis = ifat.perc_pis
+            ifat.val_ret_pis = ifat.val_pis
+        else:
+            ifat.perc_ret_pis = 0
+        if (empresa.retem_cofins == 'S') or (empresa.retem_cofins == produto.esfera):
+            ifat.perc_ret_cofins = ifat.perc_cofins
+            ifat.val_ret_cofins = ifat.val_cofins
+        else:
+            ifat.perc_ret_cofins = 0
+        if (empresa.retem_iss == 'S') or (empresa.retem_iss == produto.esfera):
+            ifat.perc_ret_iss = ifat.perc_iss
+            ifat.val_ret_iss = ifat.val_iss
+        else:
+            ifat.perc_ret_iss = 0
+        # a tabela de retencao de ir/csll é por cliente e está errada, o campo cadastrado é char e deveria ser decimal
+        if (empresa.retem_ir == 'S') or (empresa.retem_ir == produto.esfera):
+            ifat.perc_ret_ir = ifat.perc_ir
+            ifat.val_ret_ir = ifat.val_ir
+        else:
+            ifat.perc_ret_ir = 0
+        if (empresa.retem_csll == 'S') or (empresa.retem_csll == produto.esfera):
+            ifat.perc_ret_csll = ifat.perc_csll
+            ifat.val_ret_csll = ifat.val_csll
+        else:
+            ifat.perc_ret_csll = 0
+
+        ifat.valor_bruto = ifat.valor - ifat.ret_pis - ifat.ret_cofins - \
+            ifat.ret_iss - ifat.ret_ir - ifat.csll
+            
